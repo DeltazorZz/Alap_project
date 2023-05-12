50c2b8c75c6bdbbedae8d3eb0c1544841622559f
DeltazorZz <93091154+DeltazorZz@users.noreply.github.com>
2023-05-12 18:51:25 +0200

    Update web.php

--- Alap_project/routes/web.php
+++ Alap_project/routes/web.php
@@ -4,12 +4,6 @@ use App\Http\Controllers\InagtlanController;
 use App\Models\Ingatlanok;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/show', [InagtlanController::class, "index"]);
-Route::get('/ingatlanoknevvel', [InagtlanController::class, "osszesIngatlanKatnev"]);
-Route::post('/ingatlanfeltolt', [InagtlanController::class, "store"]);
-Route::delete('/ingatlantorol', [InagtlanController::class, "destroy"]);
-
 Route::get('/', function () {
-    return view('ingatlan');
+    return view('welcome');
 });
